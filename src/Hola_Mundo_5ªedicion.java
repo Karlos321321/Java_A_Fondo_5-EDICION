d30fce7b4a3c24e067b5cb8934f774adc6e00ee2
Karlos321321 <Karlos321321@gmail.com>
2025-11-08 08:41:02 +0100

    commit 5ª edicion

---
+++ src/Hola_Mundo_5ªedicion.java
@@ -0,0 +1,11 @@
+
+public class Hola_Mundo_5ªedicion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Java a Fondo 5ª Edicion !!!!");
+
+	}
+
+}
